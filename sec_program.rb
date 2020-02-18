a8308aa867c4988278200049f2030f6284bf2ecc
Krenar123 <krenardardhishta205@gmail.com>
2020-02-18 21:08:37 +0100

    Added some code with classes and methods on sec_program

--- sec_program.rb
+++ sec_program.rb
@@ -1,12 +1,41 @@
-<<<<<<< HEAD
 my_name = "Juliet"
 my_age = 19
 
 puts "My name is #{my_name} and I'm #{my_age} years old."
-=======
-puts "Hello" 
 
-x = "How is everyone today?"
-x.scan(/[aeiou]/)
-puts x
->>>>>>> 371fe542e2e4bc6bab6484c225690eec51600a56
+# Testing methods in class
+
+class Person
+
+    def initialize(name,age)
+        @name = name
+        @age = age
+    end
+    # Exc with getter and setter 
+    # We can set name and age with methods setName...
+    def setName(name)
+        @name = name
+    end
+    def setAge(age)
+        @age = age
+    end
+
+    # And we can get name and age with getName
+    def getName
+        return name
+    end
+    def getAge
+        return age
+    end
+
+    # Method that prints name and age
+    def print
+        puts "My name is #{@name} and I am #{@age} y.old"
+    end
+end
+# Using initialize method
+person = Person.new("Krenar",19)
+
+# Using set and get methods
+person.setName("Krenar")
+person.setAge(19)
