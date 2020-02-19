9d09e5ef9e412679c8742b4677ef8acc47df4b6a
Tanja Veljanovska <tanja.marathon@gmail.com>
2020-02-19 18:45:15 +0100

    changed variable name and used initialize method

--- sec_program.rb
+++ sec_program.rb
@@ -1,23 +1,19 @@
-my_name = "Juliet"
-my_age = 19
-
-puts "My name is #{my_name} and I'm #{my_age} years old."
-
 # Testing methods in class
 
 class Person
-
-    def initialize(name,age)
-        @name = name
-        @age = age
+    def initialize(name, age)
+        my_name = name
+        my_age = age
     end
+
     # Exc with getter and setter 
     # We can set name and age with methods setName...
     def setName(name)
-        @name = name
+        my_name = name
     end
+
     def setAge(age)
-        @age = age
+        my_age = age
     end
 
     # And we can get name and age with getName
@@ -30,12 +26,13 @@ class Person
 
     # Method that prints name and age
     def print
-        puts "My name is #{@name} and I am #{@age} y.old"
+        puts "My name is #{my_name} and I am #{my_age} y.old"
     end
 end
+
 # Using initialize method
-person = Person.new("Krenar",19)
+person = Person.new.initialize("Krenar",19)
 
 # Using set and get methods
-person.setName("Krenar")
-person.setAge(19)
+person.setName("Tanja")
+person.setAge(32)
